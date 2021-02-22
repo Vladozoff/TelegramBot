cf3dd3eb03959d55c4a33f7a6bfc4e7edacbddeb
vladozoff <nikitinvlad98@gmail.com>
2021-02-22 21:28:28 +0300

    v1.0

--- TGbot/TGbot/Program.cs
+++ TGbot/TGbot/Program.cs
@@ -12,9 +12,9 @@ namespace TGbot
         static public ITelegramBotClient botClient;
         static void Main(string[] args)
         {
-            botClient = new TelegramBotClient("1561413152:AAEVC7VbONJLKd0KXMm8qB97SNlxP1qJyWI");    //
+            botClient = new TelegramBotClient("1561413152:AAEVC7VbONJLKd0KXMm8qB97SNlxP1qJyWI");    
             var me = botClient.GetMeAsync().Result;
-            Console.WriteLine( $"Hello, World! I am user {me.Id} and my name is {me.FirstName}. username={me.Username}");
+            Console.WriteLine( $"Спасите меня, я бот по имени {me.FirstName}, мой UserName {me.Username}");
 
             botClient.OnMessage += Bot_OnMessage;
             botClient.StartReceiving();
@@ -31,15 +31,31 @@ namespace TGbot
                 switch (e.Message.Text)
                 {
                     case var message when message == "/start":
-                        await SendMessage(e.Message.Chat, "Привет привет");
+                        await SendMessage(e.Message.Chat, "Привет, "+e.Message.Chat.FirstName+"! Ты попал в VzBot! Здесь ты можешь узнать о моем создателе немного больше.\n Для справки введи команду: /help");
                         break; 
 
                     case var message when message == "/help":
-                        await SendMessage(e.Message.Chat, "Сейчас помогу...");
+                        await SendMessage(e.Message.Chat, "Команды:\n/info - Связь со создателем\n/skill - Скиллы создателя\n/gibhub - ссылка на GitHub\n/hh - ссылка на резюме");
                         break;
 
-                    case var message when message == "/hello":
-                        await SendMessage(e.Message.Chat, "Здарова");
+                    case var message when message == "/github":
+                        await SendMessage(e.Message.Chat, "В нём немного скудно. Но держи ссылку на GitHub: https://github.com/Vladozoff");
+                        break;
+
+                    case var message when message == "/hh":
+                        await SendMessage(e.Message.Chat, "Не знаю зачем тебе это... Но держи ссылку на hh: https://kazan.hh.ru/resume/7fa8afffff083fcb680039ed1f504379477234");
+                        break;
+
+                    case var message when message == "/info":
+                        await SendMessage(e.Message.Chat, "Почта: nikitinvlad98@gmail.com\nTelegtam: @Vladozofff \nНомер телефона: Одному боту известно:)");
+                        break;
+
+                    case var message when message == "/skill":
+                        await SendMessage(e.Message.Chat, "Навыки:\n-C#(ООП, Entity Framework)\n-SQL\n-HTML+CSS\n-IDA PRO + Ollydbg");
+                        break;
+
+                    default:
+                        await SendMessage(e.Message.Chat, "Внимание команда, не найдена!");
                         break;
                 }
                         
